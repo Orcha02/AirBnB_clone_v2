6522fea7f1b44b4e2d42486aeb7220c40d9605f8
Orcha02 <orcha202@gmail.com>
2021-09-11 23:45:35 +0000

    Add flask of /c/<text> replace _ for space

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+-> The application listens on 0.0.0.0, port 5000.
+-> Routes:
+    /: Displays 'Hello HBNB!'
+    /hbnb: display HBNB
+    /c/<text>: Displays 'C' followed by the value of <text>.
+"""
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+
+@app.route("/c/<text>")
+def c(text):
+    return "C " + text.replace("_", " ")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
