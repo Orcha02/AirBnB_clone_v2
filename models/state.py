e97e28ee43b009169709a229c5f52b0d3a534f0c
Orcha02 <orcha202@gmail.com>
2021-09-12 21:01:14 +0000

    Update FileStorage, update DBStorage and update State

--- models/state.py
+++ models/state.py
@@ -2,9 +2,22 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-
+import os
+import models
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
+
+if os.getenv("HBNB_TYPE_STORAGE") != "db":
+    @property
+    def cities(self):
+        """ Getter of cities"""
+        cities = models.storage.all(City)
+        list_cities = []
+
+        for i in cities.values():
+            if self.id == i.place_id:
+                list_cities.append(i)
+            return list_cities
