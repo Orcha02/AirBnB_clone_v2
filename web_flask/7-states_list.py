72ad74771b375cfebeab2d03c165d3ffaab63c43
Orcha02 <orcha2002@gmail.com>
2021-09-12 23:42:25 +0000

    Reorganize code and import State

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,13 +2,14 @@
 """Starts a Flask web application"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route("/states_list")
+@app.route("/states_list", strict_slashes=False)
 def states_list():
-    return render_template("7-states_list.html", states=storage.all("State"))
+    states = storage.all(State).values()
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
